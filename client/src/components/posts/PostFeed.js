81ef844bab5c6864804d6363566248b05f302670
jjayp4rk <jpark0227@gmail.com>
2018-10-30 02:09:09 -0400

    post-item key

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -5,11 +5,7 @@ import PostItem from "./PostItem";
 class PostFeed extends Component {
   render() {
     const { posts } = this.props;
-    return posts.map(post => (
-      <div>
-        <PostItem key={post._id} post={post} />
-      </div>
-    ));
+    return posts.map(post => <PostItem key={post._id} post={post} />);
   }
 }
 
